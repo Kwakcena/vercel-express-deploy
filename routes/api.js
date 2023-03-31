bc612dc3deefe3e5d4df2c047e63a89e3925c570
Kwakcena <rhkrgudwh@gmail.com>
2023-03-31 18:46:43 +0900

    send json

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const { v4 } = require('uuid');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.json({ message: "hello world!" });
 })
 
 router.get('/item/:slug', (req, res) => {
